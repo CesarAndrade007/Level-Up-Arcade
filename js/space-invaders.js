c236dfe4106517b282c6b8de4ca5dfa2e76c22f2
cesium <clopezandrade@ucdavis.edu>
2024-04-27 19:53:19 -0700

    Space Invader Update - Added movement and bullet firing to player functionality. Modified starter screen and added new assets.

--- js/space-invaders.js
+++ js/space-invaders.js
@@ -1,27 +1,54 @@
 "use strict"
 const globalContainer = document.querySelector(".game-sandbox");
+const playerSpeed = 2;
+const bulletSpeed = 1;
 
-function generateMissles(){
-
+class Player {
+    constructor(container) {
+        this.container = container;
+        this.player = new Image();
+        this.player.id = "player";
+        this.player.src = "../assets/space-invaders/enemy-game.png";
+        this.container.appendChild(this.player);
+    }
 }
 
-function missleRate(){
-
+class Enemy {
+    constructor(container){
+        this.container = container;
+        this.enemy = new Image();
+        this.enemy.className = "enemy";
+        this.enemy.src = "../assets/space-invaders/alien.png";
+        this.container.appendChild(this.enemy);
+    }
 }
 
-function spawnEnemies(){
-    let numEnemies = 4;
-    let numEnemiesPerRow = 2;
+function shootBullet() {
+    // Capture player dimensions
+    const player = document.getElementById("player");
+    const playerStyle = window.getComputedStyle(player);
+    const playerLeft = parseInt(playerStyle.left);
+    const playerBottom = parseInt(playerStyle.bottom);
+
+    // Create bullet and calculate proper placement
+    const bullet = document.createElement("div");
+    bullet.className = "bullet";
+    bullet.style.left = (playerLeft + player.clientWidth / 2) + "px";
+    bullet.style.bottom = (playerBottom + 20) + "px";
+
+    globalContainer.appendChild(bullet);
+    
+}
+
+
+function spawnEnemies(numEnemies, numEnemiesPerRow){
     const enemyContainer = document.createElement("div");
     enemyContainer.id = "enemyContainer";
 
     for(let i = 0; i < numEnemiesPerRow; i++){
         const enemyRow = document.createElement("div");
         for(let j = 0; j < numEnemies; j++){
-            const enemy = document.createElement("img");
-            enemy.id = "enemy";
-            enemy.src = "../assets/space-invaders/enemy-game.png";
-            enemyRow.appendChild(enemy);
+            const enemyObj = new Enemy(enemyRow);
         }
         enemyContainer.appendChild(enemyRow);
     }
@@ -39,13 +66,67 @@ function initializeScore(){
 function startGame(){
     const startScreen = document.getElementById("game-block");
     startScreen.classList.add("disappear");
+    
+    const container = document.querySelector('.game-sandbox');
+    const player = new Player(container);
+
     initializeScore();
-    spawnEnemies();
+    spawnEnemies(9, 3);
+
+    let moveLeft = false;
+    let moveRight = false;
+    
+    // Event Listeners added to keep track of player actions (Left, Right, and Space)
+    document.addEventListener('keydown', (event) => {
+        if(event.key === 'ArrowLeft')
+            moveLeft = true;
+        if(event.key === 'ArrowRight')
+            moveRight = true;
+        if(event.key === " ")
+            shootBullet();
+    });
+    document.addEventListener('keyup', (event) => {
+        if(event.key === 'ArrowLeft')
+            moveLeft = false;
+        if(event.key === 'ArrowRight')
+            moveRight = false;
+    });
+    
+    // Perform actions based on player keybinds
+    function movementControl() {
+        const player = document.getElementById("player");
+        const bullets = document.querySelectorAll(".bullet");
+        
+        const style = window.getComputedStyle(player);
+        const left = parseInt(style.left);
+        
+        if (moveLeft && left > 0) 
+            player.style.left = (left - playerSpeed) + "px";
+        
+        if (moveRight && (left + player.clientWidth < globalContainer.clientWidth)) 
+            player.style.left = (left + playerSpeed) + "px";
+        
+        
+        bullets.forEach((bullet) => {
+            const bottom = parseInt(bullet.style.bottom);
+            bullet.style.bottom = (bottom + bulletSpeed) + "px";
+    
+            if (bottom > globalContainer.clientHeight) {
+                bullet.remove();
+            }
+        });
+    
+        requestAnimationFrame(movementControl);
+    }
+    movementControl();
 }
 
 function restartGame(){
 
 }
+
+
+
 
 
 
